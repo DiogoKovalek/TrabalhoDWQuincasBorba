d8e03d9f15079b0658d5d6eacac21e036e19b7ce
rafa <137178422+rafa1233etuw3ar@users.noreply.github.com>
2023-12-02 15:53:02 -0300

    Add files via upload

--- trabalho de portugues/quincas site/teste.js
+++ trabalho de portugues/quincas site/teste.js
@@ -1,3 +1,31 @@
 function carregarConteudo(url) {
     document.getElementById('corpo').src = url;
 }
+
+const Menu = document.querySelector('.menu');
+const navMenu = document.querySelector('.nav-menu');
+
+let isOpen = false;
+
+Menu.addEventListener('click', function() {
+  isOpen = !isOpen;
+
+  if (isOpen) {
+    navMenu.style.left = '0';
+  } else {
+    navMenu.style.left = '-100%';
+  }
+});
+
+document.getElementById('personagens').addEventListener('click', function() {
+  var subMenu = document.getElementById('sub-menu-personagens');
+  subMenu.classList.toggle('show');
+});
+
+// Fechar o submenu se clicar fora dele
+document.addEventListener('click', function(e) {
+  if (!e.target.closest('#personagens')) {
+      var subMenu = document.getElementById('sub-menu-personagens');
+      subMenu.classList.remove('show');
+  }
+});
